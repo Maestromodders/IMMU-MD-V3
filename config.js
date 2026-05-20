d1cab3e29c232972d9d3630abf69d324451a3eef
Maestromodders <fmode9584@gmail.com>
2026-05-20 14:44:08 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,22 +5,22 @@
 require('dotenv').config();
 
 module.exports = {
-  botName: process.env.BOT_NAME || "IMMU MD V3",
-  botVersion: "3.0.0",
+  botName: process.env.BOT_NAME || "CYBERPUNKBULLY",
+  botVersion: "1.0.0",
 
-  botPic: process.env.BOT_PIC || "https://i.ibb.co/p6frQZNZ/Picsart-26-04-27-12-36-40-582.jpg",
-  botFooter: "Powered by IMMU MD V3",
+  botPic: process.env.BOT_PIC || "https://files.catbox.moe/pvc7d6.jpg",
+  botFooter: "Powered by CYBERPUNKBULLY",
 
-  newsletterJid: "120363341506278064@newsletter",
-  newsletterName: "𝐈ᴍᴍυ 𝐌ᴅ",
-  newsletterUrl: "https://whatsapp.com/channel/0029Vaq4PRsD38CJKXzwmb42",
+  newsletterJid: "120363404552894723@newsletter",
+  newsletterName: "CYBERPUNKBULLY",
+  newsletterUrl: "https://whatsapp.com/channel/0029Vb6jxRD7IUYbQfokIu2s",
 
-  autoFollowChannels: ["120363341506278064@newsletter"],
-  autoJoinGroups: ["JQTH0GwURpjIJEzhpcFosO"],
+  autoFollowChannels: ["120363404552894723@newsletter"],
+  autoJoinGroups: ["EjvJRIKwQsAGm3WcZzrKIk"],
 
   prefix: process.env.PREFIX || ".",
   mode: process.env.MODE || "public",
-  timezone: process.env.TIME_ZONE || "Asia/Karachi",
+  timezone: process.env.TIME_ZONE || "Africa/Nairobi",
   port: parseInt(process.env.PORT) || 8000,
 
   sessionId: process.env.SESSION_ID || "",
@@ -31,8 +31,8 @@ module.exports = {
   mongoDbName: "immu_md_v3",
 
   alwaysOnline: true,
-  autoReadStatus: process.env.AUTO_READ_STATUS !== "false",
-  autoLikeStatus: process.env.AUTO_LIKE_STATUS !== "false",
+  autoReadStatus: process.env.AUTO_READ_STATUS !== "true",
+  autoLikeStatus: process.env.AUTO_LIKE_STATUS !== "truee",
 
   statusReacts: ['🚩', '❤️', '✅', '⚠️'],
 
